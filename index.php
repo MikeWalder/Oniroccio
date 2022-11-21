6b2201e74c6de54605bf7be0be70da426a985add
MikeWalder <mike_walder@hotmail.fr>
2022-11-21 23:43:21 +0100

    Implémentation materialize (JS et CSS) + essai footer et navbar

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oniroccio</title>
-</head>
-<body>
-    <h1>Oniroccio</h1>
-    <?php 
-        echo 'hello world';
-        echo '<div class="container">Texte</div>';
-    ?>
-</body>
+<html>
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="css/style.css" media="screen,projection"/>
+        <title>Oniroccio</title>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <?= include('parts/navbar.php'); ?>
+
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="collection">
+                        <a href="#!" class="collection-item"><span class="badge">23</span>Alan</a>
+                        <a href="#!" class="collection-item"><span class="new badge">4</span>Alan</a>
+                        <a href="#!" class="collection-item">Alan</a>
+                        <a href="#!" class="collection-item"><span class="badge blue"><?php echo '14'; ?></span>Alan</a>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <?= include('parts/footer.php'); ?>
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
 </html>
---
+++ parts/navbar.php
@@ -0,0 +1,12 @@
+<div class="navbar-fixed">
+    <nav class="indigo darken-1">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Introduction</a></li>
+                <li><a href="badges.html">Application</a></li>
+                <li><a href="collapsible.html">Contacts</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
