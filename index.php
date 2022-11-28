02c9dc1ba41394a9cb45dcc82fb0f4a89af4ff25
MikeWalder <mike_walder@hotmail.fr>
2022-11-28 23:50:05 +0100

    Ajout cards et contenu pour tests

--- index.php
+++ index.php
@@ -12,18 +12,55 @@
     </head>
     <body>
         <?= include('parts/navbar.php'); ?>
+        <main id="main">
+            <!-- Main content of our page -->
+            <div class="row">
+                <a class="btn-small">Button</a>
+            </div>
 
-        <main>
-            <div class="container">
-                <div class="row">
-                    <div class="collection">
-                        <a href="#!" class="collection-item"><span class="badge">23</span>Alan</a>
-                        <a href="#!" class="collection-item"><span class="new badge">4</span>Alan</a>
-                        <a href="#!" class="collection-item">Alan</a>
-                        <a href="#!" class="collection-item"><span class="badge blue"><?php echo '14'; ?></span>Alan</a>
+            <div class="row">
+                <div class="col s6 m4">
+                    <div class="card large">
+                        <div class="card-image">
+                            <img src="content/images/justinien.jpg" alt="Justinien 1er">
+                            <span class="card-title flow-text">Justinien 1er</span>
+                            <a href="" class="btn-floating btn-large halfway-fab waves-effect waves-light indigo darken-1"><i class="material-icons">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p class="flow-text">Justinien 1st was the most famous byzantine emperor.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s6 m4">
+                    <div class="card large">
+                        <div class="card-image" >
+                            <img src="content/images/basile2.jpg" alt="Basile 2">
+                            <span class="card-title flow-text">Basile 2</span>
+                            <a href="" class="btn-floating btn-large halfway-fab waves-effect waves-light indigo darken-1" style="overflow: visible"><i class="material-icons" style="overflow: visible">add</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p class="flow-text">Basile 2 was a Byzantine emperor from the 11th century.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s6 m4">
+                    <div class="card large">
+                        <div class="card-image">
+                            <img src="content/images/alexis1er.jpg" alt="Alexis 1er Comnène">
+                            <span class="card-title flow-text">Alexis 1er Comnène</span>
+                            <a href="" class="btn-floating btn-large halfway-fab waves-effect waves-light indigo darken-1">
+                                <i class="material-icons">add</i>
+                            </a>
+                        </div>
+                        <div class="card-content">
+                            <p class="flow-text">Alexis Comnene the first 2 was a Byzantine emperor from the 13th century. 
+                                He tried to reform the empire despite the arab attacks and won some successes.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+            
         </main>
 
         <?= include('parts/footer.php'); ?>
